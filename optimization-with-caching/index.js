9bde3074d137f59b8fcd02d60882a2781ffdf197
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-20 11:01:51 -0700

    Added times10 and memoTimes10 with tests

--- optimization-with-caching/index.js
+++ optimization-with-caching/index.js
@@ -4,7 +4,10 @@
 //input: [1,5,2,1] => output: [1,2,5]
 //input: [4,2,2,3,2,2,2] => output: [2,3,4]
 module.exports = {
-    uniqSort
+    uniqSort,
+    factorial,
+    times10,
+    memoTimes10
 };
 
 function uniqSort(arr) {
@@ -17,3 +20,29 @@ function uniqSort(arr) {
     return [...breadcrumbs].sort((a, b) => a - b);
 };
 
+function factorial(n){
+
+};
+
+// Task 1: Write a function, times10, that takes an argument, n, and multiples n times 10
+// a simple multiplication fn
+function times10(n){
+    return n * 10;
+};
+
+
+// Task 2: Use an object to cache the results of your times10 function. 
+// protip 1: Create a function that checks if the value for n has been calculated before.
+// protip 2: If the value for n has not been calculated, calculate and then save the result in the cache object.
+
+const cache = {};
+
+function memoTimes10(n){
+    if (cache[n]){
+        return cache[n];
+    } else {
+        cache[n] = times10(n);
+    }
+
+    return cache[n];
+};
--- optimization-with-caching/test.js
+++ optimization-with-caching/test.js
@@ -1,16 +1,46 @@
-const uniq = require('./index');
+const caching = require('./index');
 
 describe('uniqSort', () => {
     it('is defined', () => {
-        expect(uniq.uniqSort).toBeDefined();
+        expect(caching.uniqSort).toBeDefined();
     });
 
     it('returns a unique sorted array', () => {
         const expected = [2,3,4];
-        const actual = uniq.uniqSort([2,3,4,2,3,4]);
+        const actual = caching.uniqSort([2,3,4,2,3,4]);
 
         expect(actual).toStrictEqual(expected);
     });
+});
 
-    
-})
+describe('factorial', () => {
+    it('is defined', () => {
+        expect(caching.factorial).toBeDefined();
+    });
+});
+
+describe('times10', () => {
+    it('is defined', () => {
+        expect(caching.times10).toBeDefined();
+    });
+
+    it('multiplies input by 10', () => {
+        expect(caching.times10(2)).toBe(20);
+        expect(caching.times10(20)).toBe(200);
+        expect(caching.times10(5)).toBe(50);
+        expect(caching.times10(100)).toBe(1000);
+    });
+});
+
+describe('memoTimes10', () => {
+    it('is defined', () => {
+        expect(caching.memoTimes10).toBeDefined();
+    });
+
+    it('multiplies input by 10', () => {
+        expect(caching.memoTimes10(2)).toBe(20);
+        expect(caching.memoTimes10(20)).toBe(200);
+        expect(caching.memoTimes10(5)).toBe(50);
+        expect(caching.memoTimes10(100)).toBe(1000);
+    });
+});
