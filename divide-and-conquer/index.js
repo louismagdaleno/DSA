1a9620c1286e84861c32bb1459cf584ec213bde4
Louis Magdaleno <buildthatapp@gmail.com>
2019-06-21 10:43:11 -0700

    Reimplemented binary search from memory for clarity

--- divide-and-conquer/index.js
+++ divide-and-conquer/index.js
@@ -15,6 +15,39 @@ function linearSearch(list, item){
     return index;
 }
 
+// create min and max variables to hold indices
+// create midpoint variable to hold midpoint index at each step
+
+// while min <= nax
+
+// update midpoint to be = min + max / 2 rounded down
+
+// if the value of list at midpoint is the item, return it
+// else check if value of list at midpoint is < item, increment min to be midpoint + 1
+// else set max to be midpoint - 1
+// if we reach the end of the function, there is no match, return -1
+function bSearch(list, item){
+    let min = 0;
+    let max = list.length - 1;
+    let midpoint;
+
+    while ( min <= max){
+        midpoint = Math.floor((min + max) / 2);
+
+        if (list[midpoint] === item){
+            return item;
+        } else {
+            if (list[midpoint] < item){
+                min = midpoint + 1;
+            } else {
+                max = midpoint - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
 function binarySearch(list, item){
     let min = 0;
     let max = list.length - 1;
